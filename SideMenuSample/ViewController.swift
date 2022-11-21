040bfe0c8598f555cb427c7ff2349984213ea003
Gov <govardhan.sgt@gmail.com>
2022-11-21 15:23:30 +0530

    Initial Commit

---
+++ SideMenuSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenuSample
+//
+//  Created by Prathap Reddy on 21/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
